07f2c157d2a1050c7eaf4d415645a5d032dab350
lvd536 <nbikiti@gmail.com>
2025-02-07 22:22:36 +0400

    Add Project Files

---
+++ ChatManager/Program.cs
@@ -0,0 +1,46 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("7371147310:AAEwln2CDIWVzYNTFHMdUwbzyzHod1qgDDQ", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnMessage += OnMessage;
+bot.OnUpdate += OnCallbackQuery;
+bot.OnError += OnError;
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel();
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is null) return;
+    var commandParts = msg.Text.Split(' ');
+    var command = commandParts[0];
+    var argument = commandParts.Length >= 2 ? commandParts[1] : null;
+    var defArgument = commandParts.Length >= 3 ? commandParts[2] : null;
+    if (msg.Text.StartsWith('/'))
+    {
+        switch (command)
+        {
+
+        }
+    }
+
+    Console.WriteLine($"[Debug] Получено {type} '{msg.Text}' в {msg.Chat}");
+}
+
+async Task OnCallbackQuery(Update update)
+{
+    if (update.Type != UpdateType.CallbackQuery) return;
+    switch (update.CallbackQuery?.Data)
+    {
+    }
+}
+
+async Task OnError(Exception exception, HandleErrorSource handler)
+{
+    Console.WriteLine(exception);
+    await Task.Delay(2000, cts.Token);
+}
