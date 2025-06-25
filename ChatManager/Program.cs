b1a37543d270729cad4a8b3a32a6299ddc6b70e7
lvd. <nbikiti@gmail.com>
2025-06-25 21:33:42 +0400

    Add new bot token get type

--- ChatManager/Program.cs
+++ ChatManager/Program.cs
@@ -1,23 +1,43 @@
-﻿using ChatManager.Services;
+﻿using System.Reflection;
+using ChatManager.Services;
+using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
-using Telegram.Bot.Polling;
 
 namespace ChatManager;
 internal class Program
 {
     private static CancellationTokenSource _cts = null!;
     private static TelegramBotClient _bot = null!;
+    
     private static async Task Main()
     {
+        var executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var projectRoot = Path.GetFullPath(Path.Combine(executableLocation, @"..\..\.."));
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(projectRoot)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+        
+        var botToken = configuration.GetSection("BotConfiguration:launchType").Value == "1" ? configuration.GetSection("BotConfiguration:prodToken").Value : configuration.GetSection("BotConfiguration:devToken").Value;
+        Console.WriteLine($"Token: {botToken}");
+        if (string.IsNullOrEmpty(botToken))
+        {
+            throw new ArgumentNullException("Bot token is not configured in appsettings.json");
+        }
+
         _cts = new CancellationTokenSource();
-        _bot = new TelegramBotClient("", cancellationToken: _cts.Token);
+        _bot = new TelegramBotClient(botToken, cancellationToken: _cts.Token);
+        
         var onMessageService = new OnMessageService(_bot);
         var onCallbackQuery = new OnCallbackQueryService(_bot);
         var onErrorService = new OnErrorService(_cts, _bot);
+        
         var me = await _bot.GetMe();
         _bot.OnMessage += onMessageService.OnMessage;
         _bot.OnUpdate += onCallbackQuery.OnCallbackQuery;
         _bot.OnError += onErrorService.OnError;
+        
         Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
         Console.ReadLine();
         _cts.Cancel();
